e4429e165b88ee44b30f8b16ce0027d7420968ff
LilWinx <lil.winx@gmail.com>
2022-03-23 19:11:25 +1100

    add relative paths to base_test

--- base_test.py
+++ base_test.py
@@ -4,35 +4,32 @@ import test_learning
 import fnaconvert
 import position_product
 import position_protein
-import input
+import os
+
+testfasta = os.path.join(os.path.dirname(__file__), r"data\NC_045512.2.fasta")
+testgbk = os.path.join(os.path.dirname(__file__), r"data\NC_0045512.2-mod.gbk")
 
 class TestSum(unittest.TestCase):
-    def test_fnaconvert(self):
-        result = test_learning.fna_test(r'C:\Users\Winkie\Downloads\test.fasta')
-        self.assertEqual(result, "ATTAAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTCTTGTAGATCTGTTCTCTAAA")
+    def setUp(self):
+        self.testfasta = testfasta
+        self.testgbk = testgbk
 
-    def test_selective_removal(self):
-        result = fnaconvert.fnaconvert(r'C:\Users\Winkie\Downloads\test1.fasta')
-        self.assertNotEqual(result, ">")
-
+    def test_position_product_input_2(self):
+        result = position_product.position_product(30000, self.testgbk, self.testfasta)
+        self.assertEqual(result, "outside of genome")
+    
     def test_position_nucleotide_input(self):
-        result = position_nucleotide.position_nucleotide(11670, r'C:\Users\Winkie\Downloads\MN908947-3.fasta')
+        result = position_nucleotide.position_nucleotide(11670, self.testfasta)
         self.assertEqual(result, "G")
 
-    def test_position_product_input(self):
-        result = position_product.position_product(11670, r'C:\Users\Winkie\Downloads\MN908947-3.gbk', r'C:\Users\Winkie\Downloads\MN908947-3.fasta')
-        self.assertEqual(result, "orf1ab polyprotein")
-    
-    def test_position_product_input_2(self):
-        result = position_product.position_product(30000, r'C:\Users\Winkie\Downloads\MN908947-3.gbk', r'C:\Users\Winkie\Downloads\MN908947-3.fasta')
-        self.assertEqual(result, "outside of genome")
-
     def test_fnaconvert_fna_length(self):
-        result = fnaconvert.fna_length(r'C:\Users\Winkie\Downloads\MN908947-3.fasta')
+        result = fnaconvert.fna_length(self.testfasta)
         self.assertEqual(result, 29903)
-    
+
     def test_position_protein(self):
-        result = position_protein.position_protein(28321, r'C:\Users\Winkie\Downloads\MN908947-3.gbk')
+        result = position_protein.position_protein(28321, self.testgbk)
         self.assertEqual(result, "T")
-    
-    
+
+    def test_position_product_input(self):
+        result = position_product.position_product(11670, self.testgbk, self.testfasta)
+        self.assertEqual(result, "nsp6")
